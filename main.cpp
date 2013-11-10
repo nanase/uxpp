67ae3303d069f41c9d5ed3d6cd25246d6db60e22
Tomona Nanase <nanase@users.noreply.github.com>
2013-11-10 12:24:20 +0900

    NetBeansのプロジェクトを作成

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: nanase
+ *
+ * Created on 2013/11/10, 9:51
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  return 0;
+}
+
