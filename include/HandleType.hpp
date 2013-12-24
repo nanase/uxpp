7a82f439e2c7dfe1d4457f8700423c4041916484
Tomona Nanase <nanase@users.noreply.github.com>
2013-12-24 11:33:25 +0900

    HandleTypeクラスのusingディレクティブを削除

--- include/HandleType.hpp
+++ include/HandleType.hpp
@@ -29,8 +29,6 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <string>
 #include "EnumBase.hpp"
 
-using namespace std;
-
 namespace uxpp {
 
   /**
@@ -124,10 +122,10 @@ namespace uxpp {
     // </editor-fold>
 
   private:
-    static const unordered_map<string, values> map;
+    static const std::unordered_map<std::string, values> map;
   };
 
-  const unordered_map<string, HandleType::values>
+  const std::unordered_map<std::string, HandleType::values>
           HandleType::map = {
     {"zeroGate", zeroGate},
     {"reset", reset},
